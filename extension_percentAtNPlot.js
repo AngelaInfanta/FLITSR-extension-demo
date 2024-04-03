f0772023b34283680c86a9dbaa0bf17d113c0f7a
AngelaInfanta <airjerome_m22@ce.vjti.ac.in>
2024-04-03 15:14:35 +0530

    ploting file changes

--- extension_percentAtNPlot.js
+++ extension_percentAtNPlot.js
@@ -3,20 +3,22 @@
 const { exec } = require('child_process');
 const vscode = require('vscode');
 
-function inputPercent_at_n_file(dir, fileName, metrics, flitsrs, mode, linearLog, all, outputChannel){
-var percentAtNCommand, plotCommand;
+function inputPercent_at_n_file(VENV_PATH, env, dir, fileName, metrics, flitsrs, mode, linearLog, all, outputChannel){
+var percentAtNCommand;
 
 if (all == 'all'){
-    percentAtNCommand = `python3 /home/angela/Desktop/FLITSR/flitsr/flitsr/percent_at_n.py  "$@" plot ${fileName} ${mode} ${linearLog} ${all}`;
-    plotCommand = `python3 /home/angela/Desktop/FLITSR/flitsr/flitsr/plot.py "$@" ${fileName} ${mode} ${linearLog} ${all}`;    
-    vscode.window.showInformationMessage(`Command ran : percent_at_n plot perc_at_n_results ${mode} ${linearLog} ${all}`);
+    percentAtNCommand = `${VENV_PATH} -m flitsr.percent_at_n plot "$@" ${fileName} ${linearLog} ${all}`;
+    vscode.window.showInformationMessage(`Command ran : percent_at_n plot ${fileName} ${linearLog} ${all}`);
+}
+else if (mode == 'mode'){
+    percentAtNCommand = `${VENV_PATH} -m flitsr.percent_at_n plot "$@" ${fileName} ${mode} metrics=[${metrics}] ${linearLog}`;
+    vscode.window.showInformationMessage(`Command ran : percent_at_n plot ${fileName} ${mode} metrics=[${metrics}] ${linearLog}`);
 }
 else {
-    percentAtNCommand = `python3 /home/angela/Desktop/FLITSR/flitsr/flitsr/percent_at_n.py  "$@" plot  ${fileName} metrics=[${metrics}] flitsrs=[${flitsrs}] ${mode} ${linearLog}`;
-    plotCommand = `python3 /home/angela/Desktop/FLITSR/flitsr/flitsr/plot.py "$@" ${fileName} metrics=[${metrics}] flitsrs=[${flitsrs}] ${mode} ${linearLog}`;
-    vscode.window.showInformationMessage(`Command ran : percent_at_n plot perc_at_n_results metrics=[${metrics}] flitsrs=[${flitsrs}] ${mode} ${linearLog}`);
+    percentAtNCommand = `${VENV_PATH} -m flitsr.percent_at_n plot "$@" ${fileName} metrics=[${metrics}] flitsrs=[${flitsrs}] ${linearLog}`;
+    vscode.window.showInformationMessage(`Command ran : percent_at_n plot ${fileName} metrics=[${metrics}] flitsrs=[${flitsrs}] ${linearLog}`);
 }
-exec(percentAtNCommand, { cwd: dir }, (error1, stdout1, stderr1) => {
+exec(percentAtNCommand, { cwd: dir, env:env }, (error1, stdout1, stderr1) => {
     if (error1) {
         outputChannel.appendLine(`Error executing percent_at_n script: ${error1}`);
         return;
@@ -27,21 +29,6 @@ exec(percentAtNCommand, { cwd: dir }, (error1, stdout1, stderr1) => {
     if (stdout1) {
         outputChannel.appendLine(`Std Out (percent_at_n): ${stdout1}`);
     }
-
-    exec(plotCommand, { cwd: dir }, (error2, stdout2, stderr2) => {
-        if (error2) {
-            outputChannel.appendLine(`Error executing plot script: ${error2}`);
-
-        }
-        if (stderr2) {
-            outputChannel.appendLine(`Std Error (plot): ${stderr2}`);
-            return;
-        }
-        if (stdout2) {
-            outputChannel.appendLine(`Std Out (plot): ${stdout2}`);
-        }
-        outputChannel.appendLine("----------------------------------------------- Done ---------------------------------------------");
-    });
 });
 }
 
